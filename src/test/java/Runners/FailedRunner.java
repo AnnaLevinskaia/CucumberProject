0eaa5fbcafe0e046aab98255d52916c8a8c8668a
AnnaLevv <anyaa_dms@mail.ru>
2023-01-18 16:13:41 -0500

    API part

--- src/test/java/Runners/FailedRunner.java
+++ src/test/java/Runners/FailedRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
 
         //dryRun = false, //false - start execution again
         //tags="@sprint3 or @sprint1",
-        //tags = "@tc1101",
+        tags = "@tc1101",
         //to remove irrelevant information from console, you need to set monochrome to true
         monochrome = true,
         //to generate the report we need plugin of runner class
